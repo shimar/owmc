428666f6bb9ddfdb85ccf4d8d4d84b0b28d13fad
shimar <shima01dev@gmail.com>
2015-07-21 00:24:43 +0900

    now we use the jade template engine and replace index.html to it.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 'use strict';
 
 var gulp = require('gulp');
+var jade = require('gulp-jade');
+
+gulp.task('jade', function() {
+  gulp.src('renderer/views/jade/**/*.jade')
+  .pipe(jade({
+    pretty: true
+  }))
+  .pipe(gulp.dest('renderer/views'));
+});
 
 gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 
