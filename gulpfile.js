5a801c1269aef6ea17fe123ebd3ca382ece9e26f
shimar <shima01dev@gmail.com>
2015-07-21 23:06:51 +0900

    add the main less file and write gulp task to compile it.

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,13 @@ gulp.task('jade', function() {
   .pipe(gulp.dest('renderer/views'));
 });
 
+gulp.task('styles', function() {
+  return gulp.src('renderer/styles/less/main.less')
+         .pipe($.plumber())
+         .pipe($.less({ paths: ['.'] }))
+         .pipe(gulp.dest('renderer/styles'));
+});
+
 gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 
 gulp.task('default', ['clean'], function () {
