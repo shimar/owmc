84833dd3d9e4f970ab7fec407bf7906767a58a72
shimar <shima01dev@gmail.com>
2015-07-26 23:06:28 +0900

    add log.

--- renderer/controllers/main_controller.js
+++ renderer/controllers/main_controller.js
@@ -5,10 +5,11 @@ var ctrl = function($scope, OpenWeatherMap) {
   $scope.weather = null;
   var params = {
     id:    1859642,             // kawasaki
-    APPID: '26695d71e886d9477a5ac106cbc2e631'       // appid
+    APPID: 'Your API Key'       // appid
   };
   OpenWeatherMap.weather(params).then(function(data) {
     $scope.weather = data;
+    console.log($scope.weather);
   });
 
 };
