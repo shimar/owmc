ff3e40e1a7629948eb189123a1033df7311723b2
shimar <shima01dev@gmail.com>
2015-08-12 11:04:41 +0900

    refactored the scope structures.

--- renderer/controllers/main_controller.js
+++ renderer/controllers/main_controller.js
@@ -7,23 +7,31 @@ var ctrl = function($scope, OpenWeatherMap) {
   $scope.center  = {
     zoom: 12
   };
+
   $scope.defaults = {
     controls: {
       zoom: false
     }
   };
 
-  var params = {
+  $scope.params = {
     id:    1859642,             // kawasaki
     APPID: 'Your API KEY'
   };
 
-  OpenWeatherMap.weather(params).then(function(data) {
-    $scope.weather = data;
-    $scope.center.lat = data.coord.lat;
-    $scope.center.lon = data.coord.lon;
-    $scope.weatherLoaded = true;
-  });
+  /**
+   * OpenWeatherMap APIをコールし、天気情報をロードする。
+   */
+  $scope.load = function() {
+    OpenWeatherMap.weather($scope.params).then(function(data) {
+      $scope.weather    = data;
+      $scope.center.lat = data.coord.lat;
+      $scope.center.lon = data.coord.lon;
+      $scope.weatherLoaded = true;
+    });
+  };
+  $scope.load();
+
 
 };
 
