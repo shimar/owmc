fd75bdf5a89a621e5729d71070ac42bbf57f50a2
shimar <shima01dev@gmail.com>
2015-08-20 19:56:36 +0900

    setup the directories and sources.

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,26 @@ var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
 
 gulp.task('styles', function() {
-  return gulp.src('renderer/styles/less/main.less')
+  return gulp.src('renderer/src/less/main.less')
          .pipe($.plumber())
          .pipe($.less({ paths: ['.'] }))
          .pipe(gulp.dest('renderer/styles'));
+});
+
+gulp.task('browserify', function() {
+  return gulp.src('src/scripts/*.js')
+    .pipe($.browserify([
+      transform: ['reactify']
+    ]))
+    .pipe($.rename('bundle.js'))
+    .pipe(gulp.dest('renderer/js'));
 });
 
 gulp.task('watch', function() {
   gulp.watch('renderer/styles/**/*.less', ['styles']);
 });
 
-gulp.task('run', [ 'styles', 'watch' ], function() {
+gulp.task('run', [ 'browserify', 'styles', 'watch' ], function() {
   return $.run('electron .').exec();
 });
 
