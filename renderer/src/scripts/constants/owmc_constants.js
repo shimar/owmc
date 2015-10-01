b734a914950d6d2ae2ca8aa909715959051c480d
shimar <shima01dev@gmail.com>
2015-10-01 21:58:23 +0900

    add event types for forecast api.

--- renderer/src/scripts/constants/owmc_constants.js
+++ renderer/src/scripts/constants/owmc_constants.js
@@ -4,8 +4,15 @@ module.exports = {
   ActionTypes: keyMirror({
     GET_WEATHER:     null,
     RECEIVE_WEATHER: null,
+
     FIND_CITY:       null,
     RECEIVE_CITIES:  null,
-    UPDATE_COORD:    null
+    UPDATE_COORD:    null,
+
+    GET_FORECAST:           null,
+    RECEIVE_FORECAST:       null,
+
+    GET_DAILY_FORECAST:     null,
+    RECEIVE_DAILY_FORECAST: null
   })
 };
