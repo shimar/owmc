05290067d148e57a56aaf4fef3830d74bbce0945
shimar <shima01dev@gmail.com>
2015-10-01 20:50:09 +0900

    the search box is top of main block.

--- renderer/src/scripts/components/main_block.jsx
+++ renderer/src/scripts/components/main_block.jsx
@@ -1,13 +1,75 @@
 var React = require('react');
+var _ = require('lodash');
 
-var Map = require('./map.jsx');
+// components.
+var Map       = require('./map.jsx');
+var SearchBox = require('./search_box.jsx');
+
+// query types.
+var queryTypes = [
+  { type: 0, caption: 'City Name', placeholder: 'ex. Kawasaki,JP' },
+  { type: 1, caption: 'ID',        placeholder: 'ex. 1859642' },
+  { type: 2, caption: 'Location',  placeholder: 'ex. lat=35.9&lon=139,0' }
+];
+
+// stores.
+var WeatherStore = require('../stores/weather_store');
+
+function getState() {
+  return {
+    weather:        WeatherStore.getWeather(),
+    cities:         WeatherStore.getCities(),
+    queryText:      '',
+    queryTypeIndex: 0,
+    searching:      false
+  }
+}
 
 var MainBlock = React.createClass({
+
+  getInitialState: function() {
+    return getState();
+  },
+
+  _onUserInput: function(queryText) {
+    this.setState({queryText: queryText});
+  },
+
+  _onQueryTypeClick: function(envet) {
+    var caption = event.target.firstChild.nodeValue;
+    var selectedQueryTypeIndex = _.findIndex(queryTypes, function(item) {
+      return item.caption == caption;
+    });
+    this.setState({ queryTypeIndex: selectedQueryTypeIndex });
+  },
+
+  _onSearch: function(event) {
+    if (this.state.queryText.trim()) {
+      this.setState({ searching: true });
+      var queryType = queryTypes[this.state.queryTypeIndex];
+
+      // TODO
+      // id以外のパラメータで検索される場合は、はじめに都市をfindする。
+      // 結果リストを並べてリンクとして表示、つつくと詳細表示。
+      //if (queryType.type === 1) {
+      //  WeatherActionCreator.getWeather(queryType.type, this.state.queryText.trim());
+      //} else {
+      //  FindCityActionCreator.findCity(queryType.type, this.state.queryText.trim());
+      //}
+    }
+  },
 
   render: function() {
     return (
       <div className="container-fluid">
         <Map />
+        <SearchBox queryType={queryTypes[this.state.queryTypeIndex]}
+                  queryTypes={queryTypes}
+                  value={this.state.queryText}
+                  onUserInput={this._onUserInput}
+                  onQueryTypeClick={this._onQueryTypeClick}
+                  onSearch={this._onSearch}
+                  searching={this.state.searching} />
       </div>
     );
   }
