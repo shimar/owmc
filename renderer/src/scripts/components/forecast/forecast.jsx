995177fbd261fe7dc6a8c6e032bdc036ec4553a5
shimar <shima01dev@gmail.com>
2015-10-04 00:05:40 +0900

    add forecast item component.

--- renderer/src/scripts/components/forecast/forecast.jsx
+++ renderer/src/scripts/components/forecast/forecast.jsx
@@ -1,6 +1,23 @@
 var React          = require('react');
 var ReactPropTypes = React.PropTypes;
 
+var moment = require('moment');
+var _ = require('lodash');
+
+var OpenWeatherMapIcon = require('../../lib/openweathermap/icon');
+
+var ForecastItem = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div>{moment(this.props.item.dt * 1000).format('YYYY-MM-DD HH:MM:SS')}</div>
+        <div><img src={OpenWeatherMapIcon.icon(this.props.item.weather[0].icon)} width='25px' height='25px' /></div>
+      </div>
+    );
+  }
+});
+
+
 var Forecast = React.createClass({
 
   propTypes: {
@@ -14,9 +31,15 @@ var Forecast = React.createClass({
       );
     }
 
+    var items = [];
+    _.forEach(this.props.forecast.list, function(data) {
+      items.push( <ForecastItem key={data.dt} item={data} /> );
+    });
+
     return (
       <div className="forecast-data-block col-xs-4 col-sm-8 col-md-8">
         <div className="forecast">
+          {items}
         </div>
       </div>
     );
