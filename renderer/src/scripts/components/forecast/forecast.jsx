803da3cf089f914ecbe90470c644fad7e04aa7cf
shimar <shima01dev@gmail.com>
2015-10-03 23:16:40 +0900

    add fallback on no forecast.

--- renderer/src/scripts/components/forecast/forecast.jsx
+++ renderer/src/scripts/components/forecast/forecast.jsx
@@ -2,11 +2,23 @@ var React          = require('react');
 var ReactPropTypes = React.PropTypes;
 
 var Forecast = React.createClass({
+
+  propTypes: {
+    forecast: ReactPropTypes.object
+  },
+
   render: function() {
+    if (!this.props.forecast || Object.keys(this.props.forecast).length < 1) {
+      return (
+        <div className="forecast-data-block col-xs-4 col-sm-8 col-md-8 hide"></div>
+      );
+    }
+
     return (
-      <div className="forecast-data-block col-xs-4 col-sm-4 col-md-4">
+      <div className="forecast-data-block col-xs-4 col-sm-8 col-md-8">
       </div>
     );
   }
+
 });
 module.exports = Forecast;
