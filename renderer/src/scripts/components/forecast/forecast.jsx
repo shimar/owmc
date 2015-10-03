d0de28a4552da32a86c74adf4a9949101ee80966
shimar <shima01dev@gmail.com>
2015-10-03 22:55:44 +0900

    add forecast component.

---
+++ renderer/src/scripts/components/forecast/forecast.jsx
@@ -0,0 +1,12 @@
+var React          = require('react');
+var ReactPropTypes = React.PropTypes;
+
+var Forecast = React.createClass({
+  render: function() {
+    return (
+      <div className="col-xs-4 col-sm-4 col-md-4">
+      </div>
+    );
+  }
+});
+module.exports = Forecast;
